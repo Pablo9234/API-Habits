1489ea66abc965f7210a00d4733cf2e5678f60cd
Pablo Eduardo <pabloedu701@gmail.com>
2024-07-16 14:54:38 -0600

    Users

--- index.js
+++ index.js
@@ -50,13 +50,87 @@ const taskTypes = [
         "description": "Getting on hard work"
     }
 ]
-  
-app.get('/taskTypes',(req,res) => {
-        res.send(taskTypes)
-})
+
+const users = {
+    "todos": [
+      {
+        "id": 1,
+        "type": "chores",
+        "task": "Drink 2L of water",
+        "streak": 5,
+        "repeat": "Everyday",
+        "bg-imageID": "waterBG",
+        "description": "Stad hydrateded, stay energised"
+    },
+    {
+        "id": 2,
+        "type": "gym",
+        "task": "Workout",
+        "streak": 5,
+        "repeat": "5 days per week",
+        "bg-imageID": "gymBG",
+        "description": "Stay strong, be a man"
+    },
+    {
+        "id": 3,
+        "type": "study",
+        "task": "Study Time",
+        "streak": 5,
+        "repeat": "5 days per week",
+        "bg-imageID": "studyBG",
+        "description": "A wise man always will be prepared"
+    },
+    {
+        "id": 4,
+        "type": "leisure",
+        "task": "chill out",
+        "streak": 5,
+        "repeat": "5 days per week",
+        "bg-imageID": "chilloutBG",
+        "description": "Get energy up, back to work"
+    },
+    {
+        "id": 5,
+        "type": "work",
+        "task": "Work",
+        "streak": 5,
+        "repeat": "5 days per week",
+        "bg-imageID": "workBG",
+        "description": "Getting on hard work"
+    },
+    {
+        "id": 6,
+        "type": "health",
+        "task": "health",
+        "streak": 5,
+        "repeat": "5 days per week",
+        "bg-imageID": "healthBG",
+        "description": "Keep up health, long life"
+    }
+    ],
+    "completed": [
+      {
+        "id": "5",
+        "task": "Clean the kitchen",
+        "streak": 5,
+        "repeat": "3 days per week",
+        "icon": "kitchen",
+        "bg-imageID": "kitchenBG",
+        "description": "Wash dishes and mop the floor"
+      }
+    ]
+  }
 
 app.get('/',(req, res) => {
     res.send('<h1>hit /taskTypes to access</h1>')
+})
+
+app.get('/taskTypes',(req,res) => {
+    res.send(taskTypes)
+})
+
+app.get('/UserActivities',(req,res) => {
+    res.send(users)
 })
 
 app.listen(port, () => {
